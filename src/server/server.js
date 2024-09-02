8bd6133a9355b59c03232556ebf847547e4745a1
Ítalo <italo.varzah@gmail.com>
2024-09-02 20:25:45 -0300

    feat: Ajusta caminho pasta.

--- src/server/server.js
+++ src/server/server.js
@@ -4,6 +4,8 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 
+require('dotenv').config({ path: path.resolve(__dirname, '../../.env') }); 
+
 const mongoUrl = process.env.MONGODB_URI;
 const client = new MongoClient(mongoUrl);
 
